2ecb766291900dcb0d275d23a89e2c9eaf650916
Fengjiang Li <fengjial@andrew.cmu.edu>
2012-11-17 21:05:25 -0500

    hi

--- src/org/cmu/fastcode/yelpscrapper/YelpScrapper.java
+++ src/org/cmu/fastcode/yelpscrapper/YelpScrapper.java
@@ -13,22 +13,5 @@ import org.jsoup.nodes.Element;
  *         Created Nov 15, 2012.
  */
 public class YelpScrapper {
-	public static void main(String[] args) throws IOException {
-		String url = "http://www.yelp.com/search?find_desc=&find_loc=15134&ns=1#find_loc=new+york&start=280";
-		
-		// Get DOM from specified URL
-		Document doc = Jsoup.connect(url).get();
-		String title = doc.title();
-		System.out.println(title);
-		System.out.println(doc.text());
-		
-		Element pageElement = doc.getElementById("paginationControls");
-		System.out.println(pageElement.text());
 	
-		System.out.println(pageElement.select("a[href*=/biz/]").first());
-		System.out.println(pageElement.select("a").first());
-		System.out.println(pageElement.select("a").first().text());
-
-		System.out.println(pageElement.select("span").first());
-	}
 }
